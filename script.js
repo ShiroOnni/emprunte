1cee2ab28e53dfed4f85ca94e8730a3b663c44f9
ShiroOnni <yacinetouazi11@gmail.com>
2025-02-25 15:17:34 +0100

    Add files via upload

--- script.js
+++ script.js
@@ -13,7 +13,7 @@ document.getElementById("loginBtn").addEventListener("click", async function () 
                 timeout: 60000,
                 userVerification: "required",
                 rpId: window.location.hostname, // S'adapte au domaine actuel
-                allowCredentials: null // Empêche la demande de Passkey / QR Code
+                allowCredentials: [] // Utilise un tableau vide pour éviter le QR Code
             }
         });
 
