d0a7420c1806b086b5432036814e1f7273e0e597
ShiroOnni <yacinetouazi11@gmail.com>
2025-02-25 14:51:26 +0100

    Add files via upload

--- script.js
+++ script.js
@@ -12,12 +12,13 @@ document.getElementById("loginBtn").addEventListener("click", async function () 
                 challenge: new Uint8Array(32),
                 timeout: 60000,
                 userVerification: "required",
-                rpId: window.location.hostname
+                rpId: "emprunte.vercel.app",  // Mets le domaine exact de ton site
+                allowCredentials: [] // Empêche la demande de Passkey / QR Code
             }
         });
 
         if (credential) {
-            window.location.href = "https://pornn.com"; // Remplace par ton vrai site
+            window.location.href = "https://ton-site.com"; // Remplace par l'URL où tu veux rediriger
         }
     } catch (error) {
         console.error("Erreur d'authentification :", error);
